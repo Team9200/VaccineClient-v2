a0546f9ea8f229fc458e27054d0d3607d2dde431
Smaker <admin@smaker.kr>
2018-11-12 03:53:12 +0900

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'linearvaccine'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
